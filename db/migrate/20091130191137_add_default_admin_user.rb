ed4ed355264b1bef0accf43f16deca4a7e7da4ae
Ross Cooperman <cooperman@gmail.com>
2009-11-30 14:14:44 -0500

    added default admin user (via migration) as a starting point for a new install

---
+++ db/migrate/20091130191137_add_default_admin_user.rb
@@ -0,0 +1,15 @@
+class AddDefaultAdminUser < ActiveRecord::Migration
+  def self.up
+    User.create(
+      :name => 'Admin',
+      :email => 'nobody@nowhere.com',
+      :login => 'admin',
+      :password => 'flB8xtvAHpB5Qm12QQTMHx',
+      :password_confirmation => 'flB8xtvAHpB5Qm12QQTMHx'
+    )
+  end
+
+  def self.down
+    User.find_by_login('admin').destroy
+  end
+end
