583f711b76b5e16b6e33ce69c04e10ac55ca685f
Ross Cooperman <cooperman@gmail.com>
2009-12-01 00:44:56 -0500

    added ability for users to see each other's wishlists

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,6 +3,7 @@ class WishlistsController < ApplicationController
 
   def index
     @wishlist = current_user.wishlists.first
-    @wishlists = Wishlist.find(:all, :conditions => "id <> #{@wishlist.id}")
+    @users = User.find(:all, :conditions => "id <> #{current_user.id}", :include => [:wishlists])
+    @user = (params[:id]) ? User.find(params[:id]) : @users.first
   end
 end
--- app/helpers/wishlists_helper.rb
+++ app/helpers/wishlists_helper.rb
@@ -1,2 +1,15 @@
 module WishlistsHelper
+
+  def total_count(users)
+    users.inject(0) { |total, user| total + user.wishlists.first.items.count }
+  end
+
+  def user_options(users)
+    options = users.inject({}) { |opts, user| opts.merge Hash[user.name => user.id] }
+    if params[:id]
+      options_for_select(options, params[:id].to_i)
+    else
+      options_for_select(options)
+    end
+  end
 end
