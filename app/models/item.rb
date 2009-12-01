ff728dbd31568381b1eac2d56c0b7d3dd516be2f
Ross Cooperman <cooperman@gmail.com>
2009-12-01 14:18:52 -0500

    fix for items with a url that does not start with xxxx://

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :wishlist, :counter_cache => true
   belongs_to :buyer, :class_name => 'User'
+
+  def url=(val)
+    self[:url] = (val.match /^\w*\:\/\//) ? val : "http://#{val}"
+  end
 end
