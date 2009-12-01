bff68dcfc58ad3c6b096ed7291e1e729e26900fe
Ross Cooperman <cooperman@gmail.com>
2009-12-01 18:10:31 -0500

    removed migrations that are not really migrations (add some stuff to the database) and removed regeneration of authlogic gemspec from deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ server "www03.rosscooperman.com", :app, :web, :db, :primary => true
 desc "Link in the production database.yml and run 'rake gems:refresh_specs'"
 task :after_update_code do
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
-  run "cd #{release_path} && rake gems:refresh_specs"
 end
 
 namespace :deploy do
