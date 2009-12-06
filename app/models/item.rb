e40a39b08e0459d4eec561e299ad886ae203740e
Ross Cooperman <cooperman@gmail.com>
2009-12-06 13:38:40 -0500

    updated feedback button art and fix for small URL http:// prepending code (not respecting blank URLs)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ActiveRecord::Base
   belongs_to :buyer, :class_name => 'User'
 
   def url=(val)
-    self[:url] = (val.match /^\w*\:\/\//) ? val : "http://#{val}"
+    self[:url] = (val.match /^\w*\:\/\//) ? val : "http://#{val}" unless val.blank?
   end
 end
