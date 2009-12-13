81d05d3e5e71e0a6461e00d4b0722fe490918a5d
Ross Cooperman <cooperman@gmail.com>
2009-12-12 23:16:27 -0500

    added emoticon support for message sending

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -1,5 +1,9 @@
 $(function() {
+  var baseUrl = $('script[src*=application.js]').attr('src');
+  baseUrl = baseUrl.replace(/javascripts\/application\.js(\?\d+)?$/, '');
   $('textarea').tinymce({
+    extended_valid_elements: "img[src|alt|mce_src|title|border]",
+    document_base_url: baseUrl,
     theme: "advanced",
     plugins: "safari,emotions",
     content_css: $('link[href*=tinymce\.css]').attr('href'),
@@ -11,7 +15,8 @@ $(function() {
     theme_advanced_containers: "controls,editor",
     theme_advanced_container_controls: "bold,italic,underline,strikethrough,|," +
                                        "forecolor,backcolor,|," +
-                                       "numlist,bullist",
+                                       "numlist,bullist,|," +
+                                       "emotions",
     theme_advanced_container_controls_align: "left",
     theme_advanced_container_editor: "mceEditor",
   });
