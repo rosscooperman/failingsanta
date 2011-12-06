b89466b4a2c2f0392f2cf343497a9a084fa63ff2
Ross Cooperman <cooperman@gmail.com>
2011-12-06 00:01:02 -0500

    Removed database.yml from the repo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "failingsanta"
-set :repository,  "git@github.fs:rosscooperman/failingsanta.git"
+set :repository,  "git@github.com:rosscooperman/failingsanta.git"
 set :deploy_to,   "/var/apps/#{application}"
 
 set :scm,           :git
@@ -10,23 +10,20 @@ set :keep_releases, 10
 set :use_sudo,      false
 set :user,          "deploy"
 
+# silence an annoying warning on deployment
+set :normalize_asset_timestamps, false
+
 server "failingsanta.com", :app, :web, :db, :primary => true
 
-desc "Link in the production database.yml"
-task :after_update_code do
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+after :update_code do
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
 end
-
-namespace :deploy do
-    desc "Restarting passenger with restart.txt"
-    task :restart, :roles => :app, :except => { :no_release => true } do
-        run "touch #{current_path}/tmp/restart.txt"
-    end
-
-    [:start, :stop].each do |t|
-        desc "#{t} task is a no-op with passenger"
-        task t, :roles => :app do ; end
-    end
-end
-
-after "deploy", "deploy:cleanup"
+after :deploy, "deploy:cleanup"
