cf02969e4e29bdafdf73ddb157d25dfb7d115013
Ross Cooperman <cooperman@gmail.com>
2011-12-07 21:28:54 -0500

    Experiment to try to determine where a javascript error is coming from.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,18 @@ $(function() {
     function() { $(this).removeClass('hover'); }
   );
 
-  $("#feedback a").hover(
-    function() {
-      var image = $(this).children('img');
-      var src   = image.attr('src').replace(/(.*)\.png/, "$1_over.png");
-      image.attr('src', src);
-    },
-    function() {
-      var image = $(this).children('img');
-      var src   = image.attr('src').replace(/_over/, "");
-      image.attr('src', src);
-    }
-  );
+  // $("#feedback a").hover(
+  //   function() {
+  //     var image = $(this).children('img');
+  //     var src   = image.attr('src').replace(/(.*)\.png/, "$1_over.png");
+  //     image.attr('src', src);
+  //   },
+  //   function() {
+  //     var image = $(this).children('img');
+  //     var src   = image.attr('src').replace(/_over/, "");
+  //     image.attr('src', src);
+  //   }
+  // );
 });
 
 function ajaxCancel() {
