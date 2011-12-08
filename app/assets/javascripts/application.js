a4dcccd3892b17c8b9391829d2a79f7da9d7d6c2
Ross Cooperman <cooperman@gmail.com>
2011-12-07 21:32:30 -0500

    Experiment to try to determine where a javascript error is coming from.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery.ba-bbq.min
 //= require jquery.timers-1.2
-//= require jquery.colorbox
+// require jquery.colorbox
 //= require tinymce/jscripts/tiny_mce/jquery.tinymce
 //= require tinymce/jscripts/tiny_mce/tiny_mce
 //= require jquery.jselect
@@ -16,18 +16,18 @@ $(function() {
     function() { $(this).removeClass('hover'); }
   );
 
-  // $("#feedback a").hover(
-  //   function() {
-  //     var image = $(this).children('img');
-  //     var src   = image.attr('src').replace(/(.*)\.png/, "$1_over.png");
-  //     image.attr('src', src);
-  //   },
-  //   function() {
-  //     var image = $(this).children('img');
-  //     var src   = image.attr('src').replace(/_over/, "");
-  //     image.attr('src', src);
-  //   }
-  // );
+  $("#feedback a").hover(
+    function() {
+      var image = $(this).children('img');
+      var src   = image.attr('src').replace(/(.*)\.png/, "$1_over.png");
+      image.attr('src', src);
+    },
+    function() {
+      var image = $(this).children('img');
+      var src   = image.attr('src').replace(/_over/, "");
+      image.attr('src', src);
+    }
+  );
 });
 
 function ajaxCancel() {
